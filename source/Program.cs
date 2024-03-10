d1a609e7f84809d91fa74138c8cd54e9573e2f9b
Jess Kube <jessk@microsoft.com>
2024-03-09 23:27:42 -0800

    fixing namespace

--- source/Program.cs
+++ source/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Setup.Configuration;
 
-namespace Sledge.VsExtensionDetector
+namespace VsSetupInfo
 {
 	class Program
 	{
